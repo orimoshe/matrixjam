fd7a11618ad8f9e9d398110053f197a2b5861cf4
orimoshe0123 <orimoshe02@gmail.com>
2020-08-08 19:15:54 +0300

    Simplify codes

--- Assets/Team18/Characters/Main character/Scripts/mainPlayerController.cs
+++ Assets/Team18/Characters/Main character/Scripts/mainPlayerController.cs
@@ -8,13 +8,16 @@ namespace MatrixJam.Team18
         Animator m_Animator;
 
         [SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
-        [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;          // Amount of maxSpeed applied to crouching movement. 1 = 100%
         [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
-        [SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
         [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
         [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
         [SerializeField] private Transform m_CeilingCheck;                          // A position marking where to check for ceilings
-        [SerializeField] private Collider2D m_CrouchDisableCollider;                // A collider that will be disabled when crouching
+
+        public mainPlayerController controller;
+        public float runSpeed = 40f;
+
+        private float horizontalMove = 0f;
+        private bool jump = false;
 
         const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
         private bool m_Grounded;            // Whether or not the player is grounded.
@@ -31,9 +34,6 @@ namespace MatrixJam.Team18
         [System.Serializable]
         public class BoolEvent : UnityEvent<bool> { }
 
-        public BoolEvent OnCrouchEvent;
-        private bool m_wasCrouching = false;
-
         private void Awake()
         {
             m_Animator = gameObject.GetComponent<Animator>();
@@ -42,13 +42,15 @@ namespace MatrixJam.Team18
 
             if (OnLandEvent == null)
                 OnLandEvent = new UnityEvent();
-
-            if (OnCrouchEvent == null)
-                OnCrouchEvent = new BoolEvent();
         }
 
         private void Update()
         {
+            horizontalMove = Input.GetAxisRaw("Horizontal")*runSpeed;
+            if (Input.GetButtonDown("Jump"))
+            {
+                jump = true;
+            }
             if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
             {
                 m_Animator.SetBool("Walk", true);
@@ -57,6 +59,14 @@ namespace MatrixJam.Team18
             {
                 m_Animator.SetBool("Walk", false);
             }
+/*            if(Input.GetKey(KeyCode.W))
+            {
+                m_Animator.SetBool("Flip", true);
+            }
+            else
+            {
+                m_Animator.SetBool("Flip", false);
+            }*/
         }
 
         private void FixedUpdate()
@@ -76,6 +86,9 @@ namespace MatrixJam.Team18
                         OnLandEvent.Invoke();
                 }
             }
+            
+            controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+            jump = false;
         }
 
 
@@ -92,38 +105,8 @@ namespace MatrixJam.Team18
             }
 
             //only control the player if grounded or airControl is turned on
-            if (m_Grounded || m_AirControl)
+            if (m_Grounded)
             {
-
-                // If crouching
-                if (crouch)
-                {
-                    if (!m_wasCrouching)
-                    {
-                        m_wasCrouching = true;
-                        OnCrouchEvent.Invoke(true);
-                    }
-
-                    // Reduce the speed by the crouchSpeed multiplier
-                    move *= m_CrouchSpeed;
-
-                    // Disable one of the colliders when crouching
-                    if (m_CrouchDisableCollider != null)
-                        m_CrouchDisableCollider.enabled = false;
-                }
-                else
-                {
-                    // Enable the collider when not crouching
-                    if (m_CrouchDisableCollider != null)
-                        m_CrouchDisableCollider.enabled = true;
-
-                    if (m_wasCrouching)
-                    {
-                        m_wasCrouching = false;
-                        OnCrouchEvent.Invoke(false);
-                    }
-                }
-
                 // Move the character by finding the target velocity
                 Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
                 // And then smoothing it out and applying it to the character
